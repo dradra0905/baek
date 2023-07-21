8d6c22a12d929a03dd5198cbd2583bdaa3a9458b
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2023-07-21 21:35:46 +0900

    2447

---
+++ 2447/2447/2447.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void star(int n, int x, int y) {
+	if ((x / n) % 3 == 1 && (y / n) % 3 == 1)printf(" ");
+	else {
+		if (n / 3 == 0)printf("*");
+		else star(n / 3, x, y);
+	}
+}
+
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			star(n, i, j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
