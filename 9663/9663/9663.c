5ee87c65f61491ba8031a57a42a5b2da286d458c
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2023-07-23 18:38:23 +0900

    9663

---
+++ 9663/9663/9663.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int n, cnt = 0;
+int y[31], xpy[31], xmy[31];
+
+void queen(int m) {
+	if (m == n) {
+		cnt++;
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (y[i] || xpy[m + i] || xmy[m-i+n-1])continue;
+		y[i] = xpy[m + i] = xmy[m - i + n - 1] = 1;
+		queen(m + 1);
+		y[i] = xpy[m + i] = xmy[m - i + n - 1] = 0;
+	}
+}
+
+int main() {
+	scanf("%d", &n);
+
+	queen(0);
+
+	printf("%d\n", cnt);
+
+	return 0;
+}
