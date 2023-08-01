4b7774bd233a1506bc59df9b6dc992db09013893
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2023-08-01 17:05:26 +0900

    5597

---
+++ 5597/5597/5597.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+	int arr[31];
+	for (int i = 1; i <= 30; i++)arr[i] = 0;
+
+	for (int i = 0; i < 28; i++) {
+		int n;
+		scanf("%d", &n);
+		arr[n] = 1;
+	}
+
+	for (int i = 1; i <= 30; i++) {
+		if (!arr[i])printf("%d\n", i);
+	}
+
+	return 0;
+}
