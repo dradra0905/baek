bf17ed9aeb0bce2273c9d2c3bf974cc26e4311e2
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2023-08-19 15:07:12 +0900

    11053 using blog solve

--- 11053/11053/11053.c
+++ 11053/11053/11053.c
@@ -3,31 +3,8 @@
 #define max(X,Y) (((X) > (Y)) ? (X) : (Y))
 
 int arr[1001];
-int dp[1001][1001];
-int check[1001];
-int visited[1001];
+int dp[1001];
 int N, mx = 0, ans = 0;
-
-int fn(int n) {
-	if (n == N - 1)return 0;
-	if (check[n])return visited[n];
-
-	fn(n + 1);
-
-	int mx = 0;
-
-	for (int i = n + 1; i < N; i++) {
-		if (arr[n] < arr[i]) {
-			dp[n][i] = fn(i) + 1;
-			mx = max(mx, dp[n][i]);
-		}
-		else dp[n][i] = 0;
-	}
-
-	visited[n] = mx;
-	check[n] = 1;
-	return mx;
-}
 
 
 int main() {
@@ -36,14 +13,16 @@ int main() {
 		scanf("%d", &arr[i]);
 	}
 
-
-	fn(0);
-
 	for (int i = 0; i < N; i++) {
-		ans = max(ans, visited[i]);
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j] && dp[i] <= dp[j]) {
+				dp[i] = dp[j] + 1;
+				mx = max(mx, dp[i]);
+			}
+		}
 	}
 
-	printf("%d\n", ans + 1);
+	printf("%d\n", mx + 1);
 
 	return 0;
 }
