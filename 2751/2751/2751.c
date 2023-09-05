6845e9f638f1a5d99a6cf0896c8ca620d03bac8b
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2023-09-05 15:26:20 +0900

    2751

---
+++ 2751/2751/2751.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int static compare(const void* first, const void* second);
+
+int N;
+int arr[1000001];
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)scanf("%d", &arr[i]);
+	qsort(arr, N, sizeof(int), compare);
+
+	for (int i = 0; i < N; i++)printf("%d\n", arr[i]);
+
+	return 0;
+}
+
+int static compare(const void* first, const void* second) {
+	if (*(int*)first > *(int*)second)
+		return 1;
+	else if (*(int*)first < *(int*)second)
+		return -1;
+	else 
+		return 0;
+}
